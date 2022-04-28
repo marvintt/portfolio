2c2fee85d45d43d13ea5cac2c6df05ae803a390c
marvintt <marvintt@hotmail.com>
2022-04-28 00:14:58 -0500

    Navbar
    Create navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import Navbar from "./components/Navbar";
+
 function App() {
-  
   return (
     <div>
-      <h1 className="text-3xl font-bold">Hola</h1>
+      <Navbar />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
